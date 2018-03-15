81c2edf3480c81ee0af727aeb6ac13ea40ccfb4e
Leo Choi <choimankin@gmail.com>
2018-03-15 22:19:54 +0800

    1. Revamped Gallery Generator

---
+++ GalleryGenerator/Gallery.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+
+namespace GalleryGenerator
+{
+    internal class Gallery
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; } = "";
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; } = "";
+
+        [JsonProperty(PropertyName = "year")]
+        public int Year { get; set; }
+
+        [JsonProperty(PropertyName = "images")]
+        public List<Image> Images { get; set; } = new List<Image>();
+    }
+
+    internal class Image
+    {
+        public Image(string path)
+        {
+            if (Path.GetExtension(path).ToLowerInvariant() != ".jpg")
+                throw new Exception("Only jpg file can be processed.");
+
+            RealPath = path;
+            ThumbnailPath = Path.ChangeExtension(path, "thumb.jpg");
+        }
+
+        [JsonProperty(PropertyName = "real")]
+        public string RealPath { get; set; }
+
+        [JsonProperty(PropertyName = "thumbnail")]
+        public string ThumbnailPath { get; set; }
+
+        public void GenerateThumbnail()
+        {
+            if (File.Exists(ThumbnailPath)) return;
+
+            using (var image = SixLabors.ImageSharp.Image.Load(RealPath))
+            {
+                image.Mutate(x => x.Resize(new ResizeOptions
+                {
+                    Mode = ResizeMode.Min,
+                    Size = new Size(250, 250)
+                }));
+                image.Save(ThumbnailPath, new JpegEncoder {Quality = 85, IgnoreMetadata = true});
+            }
+        }
+
+        public void DeleteThumbnail()
+        {
+            File.Delete(ThumbnailPath);
+        }
+    }
+}
---
+++ GalleryGenerator/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace GalleryGenerator
+{
+    class Program
+    {
+        private const string ContainingDirectoryName = "galleries";
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            var targetRootDirectory = configuration["targetRootDirectory"];
+
+            try
+            {
+                Run(targetRootDirectory);
+
+                Console.WriteLine($"{DateTime.Now.ToString(CultureInfo.InvariantCulture)} Done all.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+                Console.WriteLine("Press any key to exit...");
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void Run(string targetRootDirectory)
+        {
+            var runningDirectory = Path.Combine(Directory.GetCurrentDirectory(), targetRootDirectory);
+            var neighbourDirectories = Directory.EnumerateDirectories(runningDirectory).ToList();
+
+            if (!runningDirectory.EndsWith(ContainingDirectoryName))
+            {
+                runningDirectory = neighbourDirectories.FirstOrDefault(nd => nd.EndsWith(ContainingDirectoryName)) ?? throw new Exception("Cannot find the directory \"galleries\".");
+                neighbourDirectories = Directory.EnumerateDirectories(runningDirectory).ToList();
+            }
+
+            ConvertToGalleries(neighbourDirectories);
+        }
+
+        private static void ConvertToGalleries(IReadOnlyList<string> directories)
+        {
+            var galleries = new List<Gallery>();
+
+            for (var index = 0; index < directories.Count; index++)
+            {
+                var directory = directories[index];
+                var directoryName = Path.GetFileName(directory);
+
+                if (directoryName == null) continue;
+
+                Console.WriteLine($"{DateTime.Now.ToString(CultureInfo.InvariantCulture)} Processing {directoryName}");
+
+                var year = new Regex("((?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])", RegexOptions.IgnoreCase | RegexOptions.Singleline).Match(directoryName).Groups[1].ToString();
+
+                var gallery = new Gallery
+                {
+                    Id = index + 1,
+                    Name = directoryName,
+                    Year = int.Parse(year)
+                };
+
+                var imagePaths = Directory.GetFiles(directory)
+                    .Where(path =>
+                    {
+                        var fileName = Path.GetFileName(path).ToLowerInvariant();
+
+                        var isThumb = fileName.Contains("thumb");
+                        var isJpg = fileName.Contains("jpg");
+
+                        return !isThumb && isJpg;
+                    })
+                    .ToArray();
+
+                Parallel.ForEach(imagePaths, new ParallelOptions {MaxDegreeOfParallelism = 4}, imagePath =>
+                {
+                    var image = new Image(imagePath);
+                    image.GenerateThumbnail();
+
+                    gallery.Images.Add(image);
+                });
+
+                Console.WriteLine($"{DateTime.Now.ToString(CultureInfo.InvariantCulture)} Processd {directoryName}, total {imagePaths.Length} images.");
+
+                galleries.Add(gallery);
+            }
+        }
+    }
+}
